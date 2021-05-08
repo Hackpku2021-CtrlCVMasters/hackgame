323bb1994c2b9483ee4ebfa3ac9b08ffb4fe8a6f
listerily <listerily@outlook.com>
2021-05-08 21:56:30 +0800

    added ui renderer

---
+++ pku-game-core/src/renderer/UIRenderer.h
@@ -0,0 +1,16 @@
+//
+// Created by listerily on 5/8/21.
+//
+
+#ifndef PKU_GAME_CORE_UIRENDERER_H
+#define PKU_GAME_CORE_UIRENDERER_H
+
+#include "BaseRenderer.h"
+
+class UIRenderer : public BaseRenderer
+{
+
+};
+
+
+#endif //PKU_GAME_CORE_UIRENDERER_H
