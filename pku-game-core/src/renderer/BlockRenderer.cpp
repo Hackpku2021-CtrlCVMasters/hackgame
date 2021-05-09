fb80c5439e658ed05bfdb76bb5a8a9b4b32ecb27
listerily <listerily@outlook.com>
2021-05-09 19:57:58 +0800

    move camera to engine

--- pku-game-core/src/renderer/BlockRenderer.cpp
+++ pku-game-core/src/renderer/BlockRenderer.cpp
@@ -20,29 +20,7 @@ void BlockRenderer::render(World &world)
 {
     auto const& blocks = world.getBlocks();
 
-    auto* player = world.getPlayer();
-    Camera3D camera = { 0 };
-    Vec3f cameraPosition = player->getPosition() + player->getEyePos();
-    camera.position = (Vector3){ cameraPosition.x, cameraPosition.y, cameraPosition.z};
-    float targetX = std::cos(player->getPitch()) * std::cos(player->getYaw());
-    float targetY = std::sin(player->getPitch());
-    float targetZ = std::cos(player->getPitch()) * std::sin(player->getYaw());
-    targetX += cameraPosition.x;
-    targetY += cameraPosition.y;
-    targetZ += cameraPosition.z;
-    camera.target = (Vector3){ targetX, targetY, targetZ };
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 45.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-
-    SetCameraMode(camera, CAMERA_PERSPECTIVE);
-    UpdateCamera(&camera);
-
-    BeginDrawing();
-
     ClearBackground(RAYWHITE);
-
-    BeginMode3D(camera);
 
     for(auto const& block: blocks)
     {
@@ -56,9 +34,7 @@ void BlockRenderer::render(World &world)
 
     DrawGrid(10, 1.0f);
 
-    EndMode3D();
 
-    EndDrawing();
 }
 
 BlockRenderer::BlockRenderer(RenderEngine & e) : BaseRenderer(e){
--- pku-game-core/src/renderer/RenderEngine.cpp
+++ pku-game-core/src/renderer/RenderEngine.cpp
@@ -2,12 +2,16 @@
 // Created by listerily on 5/8/21.
 //
 
+#include <util/Vec3f.h>
+#include <complex>
 #include "RenderEngine.h"
 
 #include "BlockRenderer.h"
 #include "MainApplication.h"
 #include "game/GameClient.h"
 #include "texture/TextureManager.h"
+#include "game/world/World.h"
+#include "game/entity/Player.h"
 
 RenderEngine::RenderEngine(MainApplication & application)
 {
@@ -30,7 +34,32 @@ void RenderEngine::initialize()
 
 void RenderEngine::render()
 {
-    blockRenderer->render(app->getGameClient().getWorld());
+    auto& world = app->getGameClient().getWorld();
+    auto* player = world.getPlayer();
+    Camera3D camera = { 0 };
+    Vec3f cameraPosition = player->getPosition() + player->getEyePos();
+    camera.position = (Vector3){ cameraPosition.x, cameraPosition.y, cameraPosition.z};
+    float targetX = std::cos(player->getPitch()) * std::cos(player->getYaw());
+    float targetY = std::sin(player->getPitch());
+    float targetZ = std::cos(player->getPitch()) * std::sin(player->getYaw());
+    targetX += cameraPosition.x;
+    targetY += cameraPosition.y;
+    targetZ += cameraPosition.z;
+    camera.target = (Vector3){ targetX, targetY, targetZ };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    SetCameraMode(camera, CAMERA_PERSPECTIVE);
+    UpdateCamera(&camera);
+
+    BeginDrawing();
+    BeginMode3D(camera);
+
+    blockRenderer->render(world);
+
+    EndMode3D();
+    EndDrawing();
 }
 
 TextureManager &RenderEngine::getTextureManager()
